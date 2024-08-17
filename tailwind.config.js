3f2671fc48bbe618f1c33755046f627b7d97d153
SANJOY <paulsanjoy2923@gmail.com>
2024-08-17 23:28:10 +0530

    i am batman

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontPop: ["Poppins", "Arial", "sans-serif"],
+        fontRal: ["Raleway", "Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
